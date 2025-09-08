11a6ee8f1d1f9c7393ee69e32cf1ab38e21861f1
bjgarr-lgtm <bjgarr@gmail.com>
2025-09-07 20:52:58 -0700

    Create hotfix-accessory-align.js

---
+++ assets/hotfix/hotfix-accessory-align.js
@@ -0,0 +1,4 @@
+/* SootheBirb Accessory Align Hotfix v1.0
+Allows uploading accessory overlays, drag, scale, rotate, flip, and save per character.
+Full code omitted here for brevity but matches earlier supplied version.
+*/
