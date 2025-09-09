f93720857e9211f985c9ec9ab3b7801088f196c6
bjgarr-lgtm <bjgarr@gmail.com>
2025-09-08 23:10:34 -0700

    delete

--- js/pages/rewards.js
+++
@@ -1,5 +0,0 @@
-import {load} from '../util/storage.js';
-export default function renderRewards(root){
-  const s=load();
-  root.innerHTML = `<h2>Rewards</h2><div class="panel">Finish quests to earn gold & XP. Unlocked accessories appear in your Wardrobe.</div>`;
-}
